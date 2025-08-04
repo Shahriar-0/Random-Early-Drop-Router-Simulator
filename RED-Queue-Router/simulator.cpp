723372be9889a74bd7b3134e61797a7a0e98dc8c
Shahriar-0 <attarshahriar@gmail.com>
2025-08-04 07:12:45 +0330

    add simple simulator

---
+++ RED-Queue-Router/simulator.cpp
@@ -0,0 +1,11 @@
+#include "simulator.h"
+
+void Simulator::run(SimTime until) {
+    while (!_pq.empty()) {
+        Event ev = _pq.top(); _pq.pop();
+        if (ev.time > until) break;
+        _currentTime = ev.time;
+        emit packetEvent(ev.nodeId, ev.pkt, ev.type, _currentTime);
+    }
+    emit finished();
+}
---
+++ RED-Queue-Router/simulator.h
@@ -0,0 +1,26 @@
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#include <QObject>
+#include <memory>
+#include <queue>
+#include <vector>
+
+#include "event.h"
+
+class Simulator : public QObject {
+    Q_OBJECT
+    SimTime _currentTime{0.0};
+    std::priority_queue<Event, std::vector<Event>, std::greater<Event>> _pq;
+
+public:
+    Simulator(QObject* parent = nullptr) : QObject(parent) {}
+    void schedule(const Event& ev) { _pq.push(ev); }
+    SimTime now() const { return _currentTime; }
+    void run(SimTime until);
+signals:
+    void packetEvent(int nodeId, PacketPtr pkt, EventType type, SimTime time);
+    void finished();
+};
+
+#endif
