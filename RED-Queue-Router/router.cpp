d9292f69e714e4b32b0918096c0340e0811305f2
Shahriar-0 <attarshahriar@gmail.com>
2025-08-21 04:56:39 +0330

    fix port forwarding and add some shits to handle recieve mechanism
    but she was just a plastic girl, a demonic angel or maybe an angleic
    demon
    Eloy

--- RED-Queue-Router/router.cpp
+++ RED-Queue-Router/router.cpp
@@ -6,33 +6,36 @@ Router::Router(int id, Simulator* sim) : QObject(sim), _id(id), _sim(sim) {
     connect(_sim, &Simulator::packetEvent, this, &Router::handleEvent);
 }
 
-void Router::addPort(int dest, double rate, double delay, size_t cap, double wq, double minTh, double maxTh, double maxP) {
-    _ports.emplace(dest, Port(dest, rate, delay, cap, wq, minTh, maxTh, maxP));
+void Router::addPort(int dest, int transDly, int propDly, size_t cap, double wq, double minTh, double maxTh, double maxP) {
+    _ports.emplace(dest, Port(dest, transDly, propDly, cap, wq, minTh, maxTh, maxP));
 }
 
 void Router::handleEvent(int nodeId, PacketPtr pkt, EventType type, SimTime t) {
-    if (nodeId != _id || type != EventType::ARRIVAL || !pkt) return;
+    if (!pkt) return;
 
     auto it = _ports.find(pkt->dst());
-    assert(it != _ports.end() && "Port not found"); // FIXME: if things got shitty just return idk man qt is shit
+    assert(it != _ports.end() && "port not found"); // FIXME: if things got shitty just return idk man qt is shit
     Port& port = it->second;
 
-    bool dropped = !port.queue.offer(pkt);
-    if (dropped) {
-        port.dropped++;
-        emit congestion(pkt->src());
-    }
-    else {
-        port.forwarded++;
-        emit metrics->record(pkt->dst(), port.queue.size(), port.dropped, port.forwarded, t);
-        nextHop(port, t);
+    if (type == EventType::RECEIVED)
+        receivedDst(port, pkt);
+    else if (nodeId == _id || type == EventType::ARRIVAL) {
+        bool dropped = !port.queue.offer(pkt);
+        if (dropped) {
+            port.dropped++;
+            emit congestion(pkt->src());
+            //? maybe record something as well? these fuckers didn't specify what to show
+        }
+        else {
+            port.forwarded++;
+            emit metrics->record(pkt->dst(), port.queue.size(), port.dropped, port.forwarded, t);
+            _sim->schedule({EventType::RECEIVED, t + port.transDly + port.propDly, _id, pkt});
+        }
     }
 }
 
-void Router::nextHop(Port& p, SimTime t) {
-    // FIXME: not sure but i think we should handle sending port delay here as well
-    // although that's a shitty design but since it's small i think it works
-    auto pkt = p.queue.poll();
-    double ser = pkt->size() * 8.0 / p.rate;
-    _sim->schedule({EventType::ARRIVAL, t + ser + p.delay, p.nxt, pkt});
+void Router::receivedDst(Port& p, PacketPtr pkt) {
+    auto polled = p.queue.poll();
+    assert(*polled == *pkt && "another packet is polled? wtf bro");
+    //? maybe record something as well? these fuckers didn't specify what to show
 }
--- RED-Queue-Router/router.h
+++ RED-Queue-Router/router.h
@@ -4,9 +4,9 @@
 #include <QObject>
 #include <unordered_map>
 
+#include "metrics.h"
 #include "queue.h"
 #include "simulator.h"
-#include "metrics.h"
 
 class Router : public QObject {
     Q_OBJECT
@@ -14,18 +14,18 @@ class Router : public QObject {
     Simulator* _sim;
     struct Port {
         int nxt;
-        double rate, delay; // FIXME: maybe one of them is enough idk
-        REDQueue queue;
+        int transDly, propDly;
+        REDQueue queue; // technically we only use this queue for PC3 but let's keep the structure
         int dropped = 0, forwarded = 0;
 
-        Port(int nextHop, double rate, double delay, size_t cap, double wq, double minTh, double maxTh, double maxP)
-            : nxt(nextHop), rate(rate), delay(delay), queue(cap, wq, minTh, maxTh, maxP), dropped(0), forwarded(0) {}
+        Port(int nextHop, int transDly, int propDly, size_t cap, double wq, double minTh, double maxTh, double maxP)
+            : nxt(nextHop), transDly(transDly), propDly(propDly), queue(cap, wq, minTh, maxTh, maxP), dropped(0), forwarded(0) {}
     };
     std::unordered_map<int, Port> _ports;
 
 public:
     Router(int id, Simulator* sim);
-    void addPort(int dest, double rate, double delay, size_t cap, double wq, double minTh, double maxTh, double maxP);
+    void addPort(int dest, int transDly, int propDly, size_t cap, double wq, double minTh, double maxTh, double maxP);
 
 signals:
     void congestion(int generatorId);
@@ -34,7 +34,7 @@ private slots:
     void handleEvent(int nodeId, PacketPtr pkt, EventType type, SimTime t);
 
 private:
-    void nextHop(Port& p, SimTime t);
+    void receivedDst(Port& p, PacketPtr pkt);
 };
 
 #endif
