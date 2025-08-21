f37802431fa2d9fb90eeed3117073b051be6237c
Shahriar-0 <attarshahriar@gmail.com>
2025-08-21 04:53:25 +0330

    compare packets
    plastic girl

--- RED-Queue-Router/packet.h
+++ RED-Queue-Router/packet.h
@@ -23,6 +23,17 @@ public:
     size_t size() const { return _size; }
     int src() const { return _src; }
     int dst() const { return _dst; }
+    std::string content() const { return _content; }
+
+    bool operator==(const Packet& other) const {
+        return _id == other._id &&
+               _creation_time == other._creation_time &&
+               _size == other._size &&
+               _src == other._src &&
+               _dst == other._dst &&
+               _content == other._content;
+        // although id should be enough because of static in generators but just to be safe
+    }
 };
 
 using PacketPtr = std::shared_ptr<Packet>;
